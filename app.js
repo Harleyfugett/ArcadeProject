5a1967502ef910b34c78f3e8b5c92a27a1f60aeb
Harley Fugett <harleyfugett@gmail.com>
2023-02-21 23:32:10 -0700

    Fuck this

--- app.js
+++ app.js
@@ -1,48 +1,115 @@
-let mainSectionEle = document.getElementById('gameboard');
+// let mainSectionEle = document.getElementById('gameboard');
+let gameBoardBoard = [
+[undefined, undefined, undefined],
+[undefined, undefined, undefined],
+[undefined, undefined, undefined]
+];
 
-let gameBoard = [];
+// let whosTurn = 0;
 
-let whosTurn = 0;
 
-function cellValueAdd(event) {
-    let cellNumber = event.currentTarget.cellNumber;
-    if(gameBoard[cellNumber] === undefined) {
-        if(whosTurn === 0) {
-            gameBoard[cellNumber] = 'x';
-            whosTurn = 1;
+
+for (let i= 0; i < gameBoardBoard.length; i++) {
+    let currentRow = gameBoardBoard[i];
+    let xTracker
+    let oTracker
+    for (let j = 0; j < gameBoardBoard.length; j++) {
+        if (currentRow[j] == "X") {
+            xTracker = xTracker + 1
         } else {
-            gameBoard[cellNumber] = 'y';
-            whosTurn = 0;
+            oTracker = oTracker + 1
         }
     }
-    checkWinner();
-    updateBoard();
-}
 
-function checkWinner() {
-    for (let i=0; i<=8; i++) {
-
+    if (xTracker === 3) {
+        console.log("X wins")
+    } else {
+        console.log("O wins")
     }
 }
 
-function updateBoard(event) {
-    for (let i=0; i<=8; i++) {
-        if(gameBoard[i] !== undefined) {
-            let cell = document.getElementById(i);
-            cell.innerHTML = gameBoard[i];
-        }
+for (let i = 0; i < gameBoardBoard.length; i++) {
+    let currentElement = gameBoardBoard[i]
+    console.log(currentElement)
+    for (let nestedI = 0; nestedI < currentElement.length; nestedI++) {
+        let nestedElement = currentElement[nestedI];
+        console.log(nestedElement)
     }
 }
+
+let gameBoard = ["You", undefined, "Opponent"];
+let currentPlayer = undefined;
+
+function aiMovement(event) {
+    let randomCell = Math.floor(Math.random() * 3)
+    if (gameBoard[randomCell] == undefined) {
+        gameBoard[randomCell] = "Opponent"
+        event.target.textContent = "Opponent"
+        currentPlayer = "You"
+    }
+}
+
+console.log(Array.isArray(gameBoard))
+
+
+let appEle = document.getElementById("app");
+
+
+function boardCellCallback(event) {
+    let targetIndexPosition = parseInt(event.target.classList[1])
+    gameBoard[targetIndexPosition] = currentPlayer;
+
+    if (currentPlayer == "You") {
+        event.target.textContent = currentPlayer
+        event.target.style.backgroundColor = "pink"
+        currentPlayer = "Opponent"
+    } else {
+        event.target.textContent = currentPlayer
+        event.target.style.backgroundColor = "peachpuff"
+        currentPlayer = "You"
+    }
+    console.log(gameBoard)
+
+    if (!gameBoard.includes(undefined)) {
+        let message = document.createElement("p")
+        message.textContent = "Game Over"
+        document.getElementsByTagName("body")[0].appendChild(message)
+    }
+}
+
 
 function buildGameBoard() {
-    let unorderedList = document.createElement("ul");
-    for (let numOfGridCells = 0; numOfGridCells <= 8; numOfGridCells++) {
-        let newGameBoardCell = document.createElement('li');
-        newGameBoardCell.setAttribute('id', numOfGridCells);
-        newGameBoardCell.addEventListener('click', cellValueAdd, false);
-        newGameBoardCell.cellNumber = numOfGridCells;
-        unorderedList.appendChild(newGameBoardCell);
+    for (let numOfGridCells = 0; numOfGridCells < gameBoard.length; numOfGridCells++) {
+        let newCell = document.createElement("p");
+        newCell.classList.add("cell");
+        newCell.classList.add(numOfGridCells)
+        newCell.addEventListener("click", boardCellCallback)
+        appEle.appendChild(newCell);
     }
-    mainSectionEle.appendChild(unorderedList);
 }
-window.addEventListener('DOMContentLoaded', buildGameBoard)
+
+window.addEventListener("DOMContentLoaded", buildGameBoard)
+
+// function buildGameBoard() {
+//     for (let rowIndex = 0; rowIndex < gameBoard.length; rowIndex++) {
+//         //Line 14 makes a new element called newRow and is saving it as a variable. NewRow represents new gameBoard
+//         let newRow = document.createElement("div") 
+//         for (let columnIndex = 0; columnIndex < gameBoard[rowIndex].length; columnIndex++) {
+//             let newCell = document.createElement("div")
+//             newCell.className = "cells"
+//             newCell.classList.add(columnIndex)
+//             newRow.appendChild(newCell)
+//         }
+//         mainSectionEle.appendChild(newRow)
+//     }
+// }
+// window.addEventListener("DOMContentLoaded", buildGameBoard)
+
+
+// function clickHandler(event) {
+//     console.log('button clicked');
+// }
+
+// function updateGameBoard() {
+
+// }
